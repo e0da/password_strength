1a964e0efce8d805bc4f176c40e5b94915232c90
Justin Force <justin.force@gmail.com>
2012-04-23 12:26:25 -0700

    Rewrite spec

--- spec/password_strength_spec.rb
+++ spec/password_strength_spec.rb
@@ -2,21 +2,29 @@ require 'spec_helper'
 
 class String
   
-  describe '.entropy' do
+  describe '.password_entropy and .password_strength' do
     { 
-      'aaaaaaaa' => :weak,
-      'friendlygh' => :weak,
-      'aaaaaaaA' => :weak,
-      'aaaaaaA5' => :ok,
-      'jT123456' => :ok,
-      'bottlerocket' => :ok,
-      'Very$trong?' => :strong,
-      'Boxxy2Boxxy' => :strong,
-      'write way more code' => :strong
-    }.each_pair do |password, rating|
+      'aaaaaaaa'            => [ 4+(2*7),              :weak   ],
+      'friendlygh'          => [ 4+(2*7)+(2*1.5),      :weak   ],
+      'aaaaaaaA'            => [ 4+(2*7)+3,            :weak   ],
+      'aaaaaaA5'            => [ 4+(2*7)+3+3,          :ok     ],
+      'jT123456'            => [ 4+(2*7)+3+3,          :ok     ],
+      'bottlerocket'        => [ 4+(2*7)+(4*1.5),      :ok     ],
+      'Very$trong?'         => [ 4+(2*7)+(3*1.5)+3+3,  :strong ],
+      'Boxxy2Boxxy'         => [ 4+(2*7)+(3*1.5)+3+3,  :strong ],
+      'write way more code' => [ 4+(2*7)+(11*1.5)+3,   :strong ],
 
-      it %[says %20s is %6s] % [password, rating] do
+      'a really, really, really long one that is just so very long' => [ 4+(2*7)+(12*1.5)+(39*1)+3, :strong ]
+    }.each_pair do |password, entropy_and_rating|
+
+      entropy = entropy_and_rating[0]
+      rating  = entropy_and_rating[1]
+      it %[%-20s     is %6s] % [password, rating] do
         password.password_strength.should == rating
+      end
+
+      it %[%-20s weighs %6d] % [password, entropy] do
+        password.weighted_entropy.should == entropy
       end
     end
   end
